db399b1eddde8f74ca3f3898db2ab92f264b6d8c
EB-Joel <105235872+EB-Joel@users.noreply.github.com>
2022-08-27 13:34:45 -0400

    started creating robofriends
    deleted unnecessary files

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Card = () => {
+    return(
+        <div>
+            <img alt='foto' src='https://robohash.org/tesxt?200x200'/>
+            <div>
+                <h2>James Doe</h2>
+                <p>Joemama@hotmail.wat</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
